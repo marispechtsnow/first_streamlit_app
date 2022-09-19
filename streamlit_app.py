275fa4a1d0ad97746b41536b3d7068c21e639e4b
marispechtsnow <113158955+marispechtsnow@users.noreply.github.com>
2022-09-19 20:48:39 +1200

    Update streamlit_app.py
    new lines of the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
 
 streamlit.title('My parents New Healthy Diner')
-
+streamlit.header('Breakfast Menu')
+streamlit.text('Full breakfast 01')
+streamlit.text('Full breakfast 02')
+streamlit.text('Full breakfast 03')
